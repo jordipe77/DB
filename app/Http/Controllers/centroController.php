4da010ec299265ebbf810949096f544093564a5b
CEP <galcera@hotmail.com>
2019-03-21 11:21:12 +0100

    Models i Controller

---
+++ app/Http/Controllers/centroController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\centro;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class centroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\centro  $centro
+     * @return \Illuminate\Http\Response
+     */
+    public function show(centro $centro)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\centro  $centro
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(centro $centro)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\centro  $centro
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, centro $centro)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\centro  $centro
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(centro $centro)
+    {
+        //
+    }
+}
--- app/Http/Controllers/donanteController.php
+++ app/Http/Controllers/donanteController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\donante;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class donacionController extends Controller
+class donanteController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -41,10 +42,10 @@ class donacionController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Models\donante  $donante
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(donante $donante)
     {
         //
     }
@@ -52,10 +53,10 @@ class donacionController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Models\donante  $donante
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(donante $donante)
     {
         //
     }
@@ -64,10 +65,10 @@ class donacionController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Models\donante  $donante
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, donante $donante)
     {
         //
     }
@@ -75,10 +76,10 @@ class donacionController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Models\donante  $donante
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(donante $donante)
     {
         //
     }
