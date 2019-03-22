959e6a57b7285d65939a0537bd7feafd7253e86d
CEP <galcera@hotmail.com>
2019-03-22 11:55:42 +0100

    sdass
    sdaasd

--- resources/views/templates/plantilla.blade.php
+++ resources/views/templates/plantilla.blade.php
@@ -13,15 +13,10 @@
 <script src="{{asset('js/popper.min.js')}}"></script>
 <script src="{{asset('js/bootstrap.min.js')}}"></script>
 
-<<<<<<< HEAD
-<link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
-<!--<link rel="stylesheet" type="text/css" href="/DatumBase/public/css/login.css"> -->
-=======
 <link href="css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 
 <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrapmin.css')}}">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
->>>>>>> master
 
 </head>
 <body>
