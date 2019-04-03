93f43458411896bc2363f9a3ade4ac3245d74af6
CEP <galcera@hotmail.com>
2019-04-03 13:42:42 +0200

    css

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -8,31 +8,39 @@ ADMIN MODE
 
 @section('principal')
 
-<div class="container full-background" id="tot">
+<div class="container" id="tot">
     <form action="" method="post">
-        <h2>OPCIONES DE ADMINISTRADOR</h2>
+            <h2>OPCIONES DE ADMINISTRADOR</h2>
+        <div class="row">
+            <div class="col-3">
+            </div>
+            <div class="col-6">
 
-    <div class="card-group mt-5">
-            <a href="{{url('/buscarUsuario')}}">
-        <div class="card mr-2 carta">
+                    <div class="card-group">
+                            <a href="{{url('/buscarUsuario')}}">
+                        <div class="card carta">
+                           <button type="button" class="btn bot" id="boton1" name="boton1"><strong>USUARIO</strong></button>
 
-           <button type="button" class="btn bot" id="boton1" name="boton1"><strong>USUARIO</strong></button>
-
+                        </div>
+                    </a>
+                    <a href="{{url('/buscarDonante')}}">
+                        <div class="card carta">
+                            <button type="button" class="btn bot" id="boton2" name="boton2"><strong>DONANTE</strong></button>
+                        </div>
+                    </a>
+                    <a href="{{url('/buscarDonacion')}}">
+                        <div class="card carta">
+                            <button type="button" class="btn bot" id="boton4" name="boton4"><strong>DONACIÓN</strong></button>
+                         </div>
+                    </a>
+                </div>
+                <div class="col-3">
+                    </div>
         </div>
-    </a>
-    <a href="{{url('/buscarDonante')}}">
-        <div class="card mr-2 carta">
-            <button type="button" class="btn bot" id="boton2" name="boton2"><strong>DONANTE</strong></button>
-        </div>
-    </a>
-    <a href="{{url('/buscarDonacion')}}">
-        <div class="card mr-2 carta">
-            <button type="button" class="btn bot" id="boton4" name="boton4"><strong>DONACIÓN</strong></button>
-         </div>
-    </a>
     </div>
     </form>
 </div>
+
 
 @endsection
 
