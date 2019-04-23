1abcc98230736c1915843e08edca26bf7c4ee9eb
danireyes5015@gmail.com <50138013>
2019-04-23 12:46:52 +0200

    e

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -130,10 +130,10 @@ Dashboard
             <div class="card-body">
               <div class="row no-gutters align-items-center">
                 <div class="col mr-2">
-                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">{{$administradores}}</div>
+                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Administradores</div>
                   <div class="row no-gutters align-items-center">
                     <div class="col-auto">
-                      <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">2</div>
+                      <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{$administradores}}</div>
                     </div>
                     <div class="col-auto">
                       <div class="progress progress-sm mr-2">
